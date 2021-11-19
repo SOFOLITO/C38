2927811f2caf10b9f235b4b331ef0f9e30f61f3c
SOFOLITO <89881330+SOFOLITO@users.noreply.github.com>
2021-11-18 19:20:15 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var database;
+var position;
+var formulario;
+var player;
+var game;
+var canvas;
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance =0;
+var car1, car2, car3, car4, cars;
+
+function setup(){
+ canvas = createCanvas(displayWidth - 20, displayHeight-30);
+ database = firebase.database();
+ game = new Game();
+ game.getState();
+ game.start();
+
+}
+
+function draw(){
+  if(playerCount === 4){
+    game.update(1);
+
+  }
+  if(gameState === 1){
+      clear();
+      game.play();
+      
+  }
+}
